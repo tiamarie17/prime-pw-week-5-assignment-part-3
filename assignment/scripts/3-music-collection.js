74ea867746c690a047857a1593f5da04791ce0b2
Tia Theisen <tiatheisen@gmail.com>
2022-09-17 11:25:54 -0500

    tested the showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,9 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
-let addedMusic;
- 
 
 function addToCollection(title, artist, yearPublished){
-    
-        addedMusic = {
+    let addedMusic = {
         title: title,
         artist: artist,
         yearPublished: yearPublished
@@ -15,31 +12,34 @@ function addToCollection(title, artist, yearPublished){
     return addedMusic; 
 }
 
-addToCollection('Strawberry Fields', 'The Beatles', 1968);
-console.log(addedMusic);
+let result = addToCollection('Strawberry Fields', 'The Beatles', 1968);
+console.log(result);
 
 addToCollection('Oops I did it again', 'Britney Spears', 2000);
-console.log(addedMusic);
+console.log(result);
 
 addToCollection('Lemonade', 'Beyoncé', 2016);
-console.log(addedMusic);
+console.log(result);
 
 addToCollection('Beyoncé', 'Beyoncé', 2013);
-console.log(addedMusic);
+console.log(result);
 
 addToCollection('Let it Be', 'The Beatles', 1970);
-console.log(addedMusic);
+console.log(result);
 
 addToCollection('She Wolf', 'Shakira', 2009);
-console.log(addedMusic);
+console.log(result);
 
 console.log(collection);
 
 
 function showCollection(array){
-   console.log(array.length);
-   for (let i = 0; i < array.length; i++){
-      let result = title + 'by' + artist + 'published in' + yearPublished;
+    let numberOfItems = array.length;
+    console.log(numberOfItems);
+
+    for (let i = 0; i < array.length; i++){
+        let result2 = array[i]
+        console.log(result2.title + ' by ' + result2.artist + ' published in '+ result2.yearPublished);
    }
-   console.log(result);
 }
+showCollection(collection);
