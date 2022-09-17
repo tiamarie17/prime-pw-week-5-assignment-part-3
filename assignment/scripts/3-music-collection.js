11eb9cfa66762662f0189641e20c5a63cd2a5ae7
Tia Theisen <tiatheisen@gmail.com>
2022-09-16 23:56:56 -0500

    added 6 albums of music to collection array and console logged the entire collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,15 +1,35 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+let addedMusic;
 
 function addToCollection(title, artist, yearPublished){
     
-    let addedMusic = {
-        title: '',
-        artist: '',
-        yearPublished: ''
+        addedMusic = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
     }
-    addedMusic.push(collection);
-    return collection;
+    collection.push(addedMusic);
+    return addedMusic; 
 }
 
+addToCollection('Strawberry Fields', 'The Beatles', 1968);
+console.log(addedMusic);
+
+addToCollection('Oops I did it again', 'Britney Spears', 2000);
+console.log(addedMusic);
+
+addToCollection('Lemonade', 'Beyoncé', 2016);
+console.log(addedMusic);
+
+addToCollection('Beyoncé', 'Beyoncé', 2013);
+console.log(addedMusic);
+
+addToCollection('Let it Be', 'The Beatles', 1970);
+console.log(addedMusic);
+
+addToCollection('She Wolf', 'Shakira', 2009);
+console.log(addedMusic);
+
+console.log(collection);
