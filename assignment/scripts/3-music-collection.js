eb8d771f465d04f6945d1d108e0c7f12278bfd12
Tia Theisen <tiatheisen@gmail.com>
2022-09-16 23:03:49 -0500

    created a function that creates a new object and adds new music to the end of an array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+    
+    let addedMusic = {
+        title: '',
+        artist: '',
+        yearPublished: ''
+    }
+    addedMusic.push(collection);
+    return collection;
+}
+
