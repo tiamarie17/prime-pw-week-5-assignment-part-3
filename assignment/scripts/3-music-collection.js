379eb83ab396003c490eb33f977e6c998e91eed8
Tia Theisen <tiatheisen@gmail.com>
2022-09-18 12:43:11 -0500

    updated several of the functions to include tracks

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,43 +2,123 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection(title, artist, yearPublished){
+function addToCollection(title, artist, yearPublished, tracks){
     let addedMusic = {
         title: title,
         artist: artist,
-        yearPublished: yearPublished
+        yearPublished: yearPublished,
+        tracks: tracks
     }
     collection.push(addedMusic);
+
     return addedMusic; 
 }
 
-console.log(addToCollection('Strawberry Fields', 'The Beatles', 1968));
+//Creating tracks for each album
+let track1 = {
+    name: 'Penny Lane',
+    duration: 136
+}
 
-console.log(addToCollection('Oops I did it again', 'Britney Spears', 2000));
+let track2 = {
+    name: 'Yellow Submarine',
+    duration: 120
+}
+let album1Tracks = [track1, track2];
 
-console.log(addToCollection('Lemonade', 'Beyoncé', 2016));
 
-console.log(addToCollection('Beyoncé', 'Beyoncé', 2013));
+let track3 = {
+    name: 'Lucky',
+    duration: 135
+}
 
-console.log(addToCollection('Let it Be', 'The Beatles', 1970));
+let track4 = {
+    name: 'Dont Go Knocking on My Door',
+    duration: 156
+}
+let album2Tracks = [track3, track4];
 
-console.log(addToCollection('She Wolf', 'Shakira', 2009));
+
+let track5 = {
+    name: 'Hold Up',
+    duration: 135
+}
+
+let track6 = {
+    name: 'Formation',
+    duration: 156
+}
+let album3Tracks = [track5, track6];
+
+
+let track7 = {
+    name: 'Pretty Hurts',
+    duration: 143
+}
+
+let track8 = {
+    name: 'Halo',
+    duration: 142
+}
+let album4Tracks = [track7, track8];
+
+
+let track9 = {
+    name: 'A Hard Days Night',
+    duration: 135
+}
+let track10 = {
+    name: 'Dear Prudence',
+    duration: 156
+}
+let album5Tracks = [track9, track10];
+
+let track11 = {
+    name: 'Loca',
+    duration: 195
+}
+let track12 = {
+    name: 'Bicicleta',
+    duration: 155
+}
+let album6Tracks = [track11, track12];
+
+
+
+//Testing addtoCollection function
+
+console.log(addToCollection('Strawberry Fields', 'The Beatles', 1968, album1Tracks ));
+
+console.log(addToCollection('Oops I did it again', 'Britney Spears', 2000, album2Tracks));
+
+console.log(addToCollection('Lemonade', 'Beyoncé', 2016, album3Tracks));
+
+console.log(addToCollection('Beyoncé', 'Beyoncé', 2013, album4Tracks));
+
+console.log(addToCollection('Let it Be', 'The Beatles', 1970, album5Tracks));
+
+console.log(addToCollection('She Wolf', 'Shakira', 2009, album6Tracks));
 
 console.log(collection);
 
 
-function showCollection(array){
-    let numberOfItems = array.length;
+//Creating showCollection function
+
+function showCollection(anyArray){
+    let numberOfItems = anyArray.length;
     console.log(numberOfItems);
 
-    for (let i = 0; i < array.length; i++){
-        let result2 = array[i]
-        console.log(result2.title + ' by ' + result2.artist + ' published in '+ result2.yearPublished);
+    for (let i = 0; i < anyArray.length; i++){
+        let result2 = anyArray[i]
+        console.log(result2.title + ' by ' + result2.artist + ' published in '+ result2.yearPublished);   
    }
-}
+   
+}  
+//Testing showCollection function
 showCollection(collection);
 
 
+//Creating findByArtist function
 function findByArtist(artist){
     let artistArray = [];
     for(let i=0; i < collection.length; i++){
@@ -49,44 +129,55 @@ function findByArtist(artist){
     }
     return artistArray;  
 }
+
+//Testing findByArtist function
+
 console.log(findByArtist('Beyoncé'));
 
 console.log(findByArtist('Justin Timberlake'));
 
 
 
+//Creating search function
+//Assuming the artist, year, and any one of the trackNames matches the criteria:
 
 function search (criteria){
     function isEmpty(criteria){
-       return Object.keys(criteria).length === 0;  
-    } 
+        return Object.keys(criteria).length === 0;  
+    }
     if(criteria == null || isEmpty(criteria)){
         return collection;
     } 
     else { 
-        let newArray = [];
         for(i=0; i<collection.length; i++){
+            let newArray = []
             let res = collection[i];
+               for (let item of res.tracks){
+                item = res.tracks[i];
             if(res.artist == criteria.artist && res.yearPublished == criteria.year){
                 newArray.push(res);
+            } else if (item == criteria.trackName) {
+                newArray.push(item);
             }
         }
-        return newArray;
+       return newArray; 
     } 
+} 
+
 }
 
 //input object that is not a match
-let mySearch = {artist: 'Ray Charles', year: 1957};    
+let mySearch = {artist: 'Ray Charles', year: 1957, trackName: 'Under Pressure'};    
 let res2 = search(mySearch);
 console.log(res2);
 //returns empty array
 
 
-//input object that is a match
-let mySearch2 = {artist: 'Britney Spears', year: 2000};
+//input object that is a match based on artist, year, and trackName
+let mySearch2 = {artist: 'Shakira', year: 2009, trackName: 'Loca'};
 let res3 = search(mySearch2);
 console.log(res3);
-//returns one Britney Spears object from collection array
+//returns newArray
 
 
 //input object that is undefined
